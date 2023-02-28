189db02c3c3850331a945d7a1a50ee945b1b1d45
Elina Pandey <elinapandey@Elinas-MacBook-Pro.local>
2023-02-28 16:21:24 +0545

    Responsive Design

---
+++ lib/Home.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_design/responsive/desktop_body.dart';
+import 'package:responsive_design/responsive/mobile_body.dart';
+import 'package:responsive_design/responsive/responsive_layout.dart';
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: const ResponsiveLayout(
+        mobileBody: MobileBody(),
+        desktopApp: DesktopBody(),),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
